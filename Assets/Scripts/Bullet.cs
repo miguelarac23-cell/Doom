254807fc8eea03ef943fdcd568b93b8e2538dfec
Eduardo340-u <eduardo.chavez@uteca.edu.mx>
2026-06-25 20:30:36 -0600

    Se agrega Mancubus y sonidos y prop de vida

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,13 +4,13 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField]
     private float speed = 20f;
-    private float damage =10f;
+    protected float damage =10f;
     public float Damage { set { damage = value; } }
-    void Awake()
+    void OnEnable()
     {
         GetComponent<Rigidbody>().linearVelocity = transform.forward * speed;
     }
-    private void OnCollisionEnter(Collision collision)
+    public virtual void OnCollisionEnter(Collision collision)
     {
         Destroy(gameObject);
     }
